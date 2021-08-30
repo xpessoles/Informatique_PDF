88b70ef9c38dc27a6fc112863a8e65233f6e616e
Xavier Pessoles <xpessoles.ptsi@free.fr>
2021-08-30 16:35:39 +0200

    Create QCM.py

---
+++ S1_Themes/TP00/QCM.py
@@ -0,0 +1,67 @@
+# QCM
+
+# Variables et types de variables
+
+# Question 1
+a = 5
+a = a+3
+print(a)
+# Je ne sais pas.
+# 3
+# 5
+# 8
+
+
+# Question 2
+a = 3
+b = 4
+a = b
+print(a)
+# Je ne sais pas.
+# 3
+# 4
+# b
+
+# Question 3
+# Quel est le type de a
+a = "64"
+print(type(a))
+# Bool
+# Integer
+# Float
+# String
+
+
+# Question 4
+
+# Question 5
+
+# Question 6
+
+# Question 7
+
+# Question 8
+
+# Question 9
+
+# Question 10
+
+# Question 11
+
+# Question 12
+
+# Question 13
+
+# Question 14
+
+# Question 15
+
+# Question 16
+
+# Question 17
+
+# Question 18
+
+# Question 19
+
+# Question 20
