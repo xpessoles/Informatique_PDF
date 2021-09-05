58874fb9488aa6ed24ea7c62d83c74c5c6f8be03
Xavier Pessoles <xpessoles.ptsi@free.fr>
2021-09-05 09:04:54 +0200

    MAJ

--- make.py
+++ make.py
@@ -735,7 +735,7 @@ for tp in info_tp:
 ####Compiler tp
 #for k in range(len(info_tp)):
 ###
-for k in range(1,2):
+for k in range(1,3):
     activite=info_tp[k]
     #rep=trouver_repertoire(activite)
     num_tp=activite[2]
