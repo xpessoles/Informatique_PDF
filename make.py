e0af8326ef09280199ba15004ecba291dd87d17f
JeanPhilippeBerne <86251842+JeanPhilippeBerne@users.noreply.github.com>
2021-10-10 10:40:31 +0200

    maj

--- make.py
+++ make.py
@@ -736,6 +736,7 @@ for tp in info_tp:
 #for k in range(len(info_tp)):
 ###
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #<<<<<<< HEAD
 #<<<<<<< Updated upstream
 #<<<<<<< Updated upstream
@@ -749,6 +750,9 @@ for tp in info_tp:
 #=======
 for k in range(3,4):
 #>>>>>>> a695b1c2ad84d31831d864d639f7a0dd05965176
+=======
+for k in range(1,3):
+>>>>>>> Stashed changes
 =======
 for k in range(1,3):
 >>>>>>> Stashed changes
