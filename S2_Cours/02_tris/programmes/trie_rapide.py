716d14f6b219df218558317b6d6fb3b734ea60e5
emiliendurif <emilien.durif@gmail.com>
2022-02-02 07:33:48 +0100

    maj tri

--- S2_Cours/02_tris/programmes/trie_rapide.py
+++ S2_Cours/02_tris/programmes/trie_rapide.py
@@ -7,7 +7,7 @@ def partition(a,g,d):
     for x in a[g+1:d]:
         if x<v:
             ainf.append(x)
-        elif x>v:
+        else:
             asup.append(x)
     a=a[0:g]+ainf+[v]+asup+a[d:len(a)]
     m=len(ainf)+g
@@ -22,12 +22,29 @@ def tri_rapide(a,g,d):
         tri_rapide(a,g,m)
         tri_rapide(a,m+1,d)
 
-
+def tri_rapide2(t:list)-> list :
+    '''Trie la liste t par une méthode récursive
+    Entrée : Une liste t
+    Sortie : La liste est modifiée et est renvoyée '''
+    if len(t) < 2 :
+        return (t)
+    else :
+        x = t[-1]
+        a=[]
+        b=[]
+        for i in range (0,len(t)-1) :
+            if t[i] < x :
+                a.append(t[i])
+            else :
+                b.append(t[i])
+        return (tri_rapide2(a) + [x] + tri_rapide2(b))
 
 
 
 #Programme principal
 a=[5,14,11,8,17,7]
+a=[5,14,11,8,8,17,7]
 #a=[4,6,3,7,5]
 #a=[7,6,4,3,2,1]
 tri_rapide(a,0,len(a))
+#tri_rapide2(a)
